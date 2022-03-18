aec6ea8ed6f48809789b03f7ad53ff8019809eee
Christopher Revell <christopher.revell@manchester.ac.uk>
2022-03-18 21:23:38 +0000

    Reading material

--- tests/smoluchowskiCoagulation.jl
+++ tests/smoluchowskiCoagulation.jl
@@ -1,3 +1,5 @@
+using DrWatson
+@quickactivate
 using ModelingToolkit
 using Catalyst
 using LinearAlgebra
@@ -12,62 +14,64 @@ Vₒ = (4π/3)*(10e-06*100)^3   # volume of a monomers in cm³
 Nₒ = 1e-06/Vₒ                # initial conc. = (No. of init. monomers) / bulk volume
 uₒ = 10000                   # No. of monomers initially
 V = uₒ/Nₒ                    # Bulk volume of system in cm³
+B = 1.53e03                  # Reaction rate parameter, s⁻¹
 
-integ(x) = Int(floor(x))
-n        = integ(N/2)
-nr       = N%2 == 0 ? (n*(n + 1) - n) : (n*(n + 1)) # No. of forward reactions
-
-## possible pairs of reactant multimers
-pair = []
-for i = 2:N
-    push!(pair,[1:integ(i/2)  i .- (1:integ(i/2))])
+# Find all possible monomer and oligomer pairs that result in oligomers with size <= N
+pairs = Tuple[]
+for i=1:N-2
+   for j=i:N-1
+       i+j<=N ? push!(pairs,(i,j)) : nothing
+   end
 end
-pair = vcat(pair...)
-vᵢ = @view pair[:,1]   # Reactant 1 indices
-vⱼ = @view pair[:,2]   # Reactant 2 indices
-volᵢ = Vₒ*vᵢ           # cm⁻³
-volⱼ = Vₒ*vⱼ           # cm⁻³
-sum_vᵢvⱼ = vᵢ .+ vⱼ  # Product index
+nReactions = length(pairs)
 
-## set i to  1 for additive kernel, 2  for constant
-i = 1
-if i==1
-    B = 1.53e03                # s⁻¹
-    kv = @. B*(volᵢ + volⱼ)/V  # dividing by volume as its a bi-molecular reaction chain
-elseif i==2
-    C = 1.84e-04               # cm³ s⁻¹
-    kv = fill(C/V, nr)
+# Additive kernel
+# Reaction rate constants proportional to sum of reactant molecular volumes
+rates = Float64[]
+for i=1:nReactions
+    push!(rates,B*(pairs[i][1]+pairs[i][2])/V)
 end
+# # Constant kernel
+# C = 1.84e-04               # cm³ s⁻¹
+# rates = fill(C/V, nReactions)
+
 
 ## state variables are X, pars stores rate parameters for each rx
 @parameters t
-@variables k[1:nr]  X[collect(1:N)](t)
-pars = Pair.(k, kv)
+@variables k[1:nReactions]  X[1:N](t)
+pars = Pair.(collect(k), rates)
 
 # time-span
-if i == 1
-    tspan = (0. ,2000.)
-elseif i == 2
-    tspan = (0. ,350.)
-end
+tspan = (0. ,2000.)
 
  # initial condition of monomers
 u₀    = zeros(Int64, N)
 u₀[1] = uₒ
-u₀map = Pair.(X, u₀)   # map variable to its initial value
+u₀map = Pair.(collect(X), u₀)    # map variable to its initial value
 
 ## vector to store the Reactions in
 rx = []
-for n = 1:nr
+for n = 1:nReactions
     # for clusters of the same size, double the rate
-    if (vᵢ[n] == vⱼ[n])
-        push!(rx, Reaction(k[n], [X[vᵢ[n]]], [X[sum_vᵢvⱼ[n]]], [2], [1]))
+    if (pairs[n][1] == pairs[n][2])
+        # Reaction(rate, [reactants], [products], [stoichiometry in], [stoichiometry out])
+        push!(rx, Reaction(pars[n], [X[pairs[n][1]]], [X[pairs[n][1]+pairs[n][2]]], [2], [1]))
     else
-        push!(rx, Reaction(k[n], [X[vᵢ[n]], X[vⱼ[n]]], [X[sum_vᵢvⱼ[n]]],
-                           [1, 1], [1]))
+        push!(rx, Reaction(pars[n], [X[pairs[n][1]], X[pairs[n][2]]], [X[pairs[n][1]+pairs[n][2]]], [1,1], [1]))
     end
 end
-rs = ReactionSystem(rx, t, X, k)
+
+# rx = []
+# for n = 1:nReactions
+#     # for clusters of the same size, double the rate
+#     if (pairs[n][1] == pairs[n][2])
+#         # Reaction(rate, reactant1, reactant2, [products], stoichiometry in, stoichiometry out )
+#         push!(rx, Reaction(k[n], [X[vᵢ[n]]], [X[sum_vᵢvⱼ[n]]], [2], [1]))
+#     else
+#         push!(rx, Reaction(k[n], [X[vᵢ[n]], X[vⱼ[n]]], [X[sum_vᵢvⱼ[n]]],[1, 1], [1]))
+#     end
+# end
+@named rs = ReactionSystem(rx, t, collect(X), collect(k))
 
 ## solving the system
 jumpsys = convert(JumpSystem, rs)
@@ -76,6 +80,7 @@ jprob   = JumpProblem(jumpsys, dprob, Direct(), save_positions=(false,false))
 jsol    = solve(jprob, SSAStepper(), saveat = tspan[2]/30)
 
 ## Results for first three polymers...i.e. monomers, dimers and trimers
+# Results for first three polymers...i.e. monomers, dimers and trimers
 v_res = [1;2;3]
 
 # comparison with analytical solution
@@ -83,17 +88,17 @@ v_res = [1;2;3]
 # to ease distinguishing it from the exact solution
 t   = jsol.t
 sol = zeros(length(v_res), length(t))
-if i == 1
+# if i == 1
     ϕ = @. 1 - exp(-B*Nₒ*Vₒ*t)
     for j in v_res
         sol[j,:] = @. Nₒ*(1 - ϕ)*(((j*ϕ)^(j-1))/gamma(j+1))*exp(-j*ϕ)
     end
-elseif i == 2
-    ϕ = @. (C*Nₒ*t)
-    for j in v_res
-        sol[j,:] = @. 4Nₒ*((ϕ^(j-1))/((ϕ + 2)^(j+1)))
-    end
-end
+# elseif i == 2
+#     ϕ = @. (C*Nₒ*t)
+#     for j in v_res
+#         sol[j,:] = @. 4Nₒ*((ϕ^(j-1))/((ϕ + 2)^(j+1)))
+#     end
+# end
 
 # plotting normalised concentration vs analytical solution
 default(lw=2, xlabel="Time (sec)")
