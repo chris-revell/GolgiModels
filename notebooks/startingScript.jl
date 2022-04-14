5e2014748b9fce2fb43527ef84f744f90096d873
chris-revell <christopher.revell@manchester.ac.uk>
2022-04-14 10:58:50 +0100

    Update startingScript.jl

--- notebooks/startingScript.jl
+++ notebooks/startingScript.jl
@@ -8,26 +8,20 @@ using SpecialFunctions
 
 ## Parameter
 nMax      = 10                      # maximum cluster size
-Vₒ        = (4π/3)*(10e-06*100)^3   # volume of a monomers in cm³
-Nₒ        = 1e-06/Vₒ                # initial conc. = (No. of init. monomers) / bulk volume
 nMonomers = 10000                   # No. of monomers initially
-V         = nMonomers/Nₒ                    # Bulk volume of system in cm³
-B         = 1.53e03                 # s⁻¹
+
 
 #
 # Kb # Budding rate per patches of membrane in a compartment
 # Km # Biochemical conversion rate for each patch of membrane (cis->medial->trans)
 # Kf # Maximum fusion rate between two compartments
 # J # Injection rate of cis-vesicles from the ER
-# kb # Budding rate normalized by the fusion rate (Kb/Kf )
-# km # Biochemical conversion rate normalized by the fusion rate (Km/Kf )
-# j # Injection rate normalized by the fusion rate (J/Kf )
+kb = 1.0 # Budding rate normalized by the fusion rate (Kb/Kf )
+km = 1.0 # Biochemical conversion rate normalized by the fusion rate (Km/Kf )
+j = 1.0 # Injection rate normalized by the fusion rate (J/Kf )
 # α # Fraction of active species (driving homotypic fusion) in the ER (cis) or the TGN (trans)
 # αᵢ # When different between boundaries, a in the boundary i (i equals ER or TGN)
 
-# kf = 1.0
-# km =
-# j =
 
 # Find all possible monomer and oligomer pairs that result in oligomers with size <= nMax
 reactantsAggregating = Tuple[]
@@ -40,20 +34,20 @@ for i=1:nMax-2
 end
 nReactionsAggregating = length(reactantsAggregating)
 # Reaction rates proportional to sum of reactant volumes
-ratesAggregating = B*Vₒ.*ones(nReactionsAggregating) # [sum(a) for a in reactantsAggregating]./V  # dividing by system volume as its a bi-molecular reaction chain
+ratesAggregating = ones(nReactionsAggregating) # [sum(a) for a in reactantsAggregating]./V  # dividing by system volume as its a bi-molecular reaction chain
 
 reactantsSplitting = collect(2:nMax)
 nReactionsSplitting = length(reactantsSplitting)
-ratesSplitting = B*Vₒ.*ones(nReactionsSplitting) # (reactantsSplitting.^2)./V
+ratesSplitting = ones(nReactionsSplitting) # (reactantsSplitting.^2)./V
 
-rateInjection = [B*Vₒ/V]
-rateRemoval = [B*Vₒ/V]
+rateInjection = [1.0]
+rateRemoval = [1.0]
 
 allRates = [rateInjection; ratesAggregating; ratesSplitting; rateRemoval]
 nReactionsTotal = length(allRates)
 
 # time-span
-tspan = (0. ,20000000.)
+tspan = (0.0 ,100000.0)
 
 # initial condition of monomers
 u₀    = zeros(Int64, nMax)
@@ -76,7 +70,7 @@ u₀map = Pair.(collect(X), u₀)   # map variable to its initial value
 # vector to store the Reactions
 rx = []
 # Add injection reaction
-push!(rx, Reaction(allRates[end], nothing, [X[1]], nothing, [1]))
+push!(rx, Reaction(k[1], nothing, [X[1]], nothing, [1]))
 # Add aggregation reactions
 for (n,pair) in enumerate(reactantsAggregating)
     push!(rx, Reaction(k[n+1], [ X[pair[1]], X[pair[2]] ], [ X[sum(pair)] ], [1, 1], [1]))
@@ -86,7 +80,7 @@ for (n,reactant) in enumerate(reactantsSplitting)
     push!(rx, Reaction(k[n+nReactionsAggregating+1], [ X[reactant] ], [ X[reactant-1], X[1] ], [1], [1, 1]))
 end
 # Add removal reaction
-push!(rx, Reaction(allRates[end], [X[1]], nothing, [1], nothing))
+push!(rx, Reaction(k[end], [X[1]], nothing, [1], nothing))
 
 
 @named rs = ReactionSystem(rx, t, collect(X), collect(k))
