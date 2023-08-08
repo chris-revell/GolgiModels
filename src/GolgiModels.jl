1eeda711255f69872064c29764c0c277e67019f7
Christopher Revell <christopher.revell@manchester.ac.uk>
2023-08-08 15:53:55 +0100

    Tidying

--- src/GolgiModels.jl
+++ src/GolgiModels.jl
@@ -48,17 +48,6 @@ module GolgiModels
     using Format
     using JSServe  
 
-    # function vecObsToFloat(a,b)
-    #     for i=1:length(a)
-    #         b[i] = a[i][]
-    #     end
-    # end
-
-    # function pairSymbolToObservable(pairs,sym,obs)
-    #     for i=1:length(pairs)
-    #         pairs[i] = Pair(sym[i],kStochFactors[i]*kObservables[i][])
-    #     end 
-
     # quickactivate("GolgiModels")
     include(srcdir("AllReactions.jl"))
     include(srcdir("GuiFigureSetup.jl"))
@@ -69,9 +58,6 @@ module GolgiModels
 
     function golgiApp(;displayFlag=true)
 
-# 11111111111111111111111        
-        # quickactivate("GolgiModels")
-         # TODO: handle this by environment variables instead
         JSServe.configure_server!(listen_port=9384, listen_url="0.0.0.0")
 
         nMax          = 20             # Max compartment size /vesicles
@@ -80,16 +66,10 @@ module GolgiModels
         ksInit        = [1.0,1.0,1.0,1.0,0.0,1.0,1.0,1.0,0.0,1.0,1.0,1.0]
         k̂             = [1.0,1.0,1.0,1.0,0.0,1.0,1.0,1.0,0.0,1.0,1.0,1.0]
         kStochFactors = [V, 1/V, 1.0, 1.0, 1.0, 1/V, 1.0, 1.0, 1.0, 1/V, 1.0, 1.0]
-# 11111111111111111111111
 
-
-# 22222222222222222222222        
         # Set up figure canvas
         fig, axCis, axMed, axTra, axDwell, parameterSliders, run, reset, linearityToggle, xLimTimeAv, linearityToggle = guiFigureSetup(ksInit)
-
-# 22222222222222222222222
     
-# 33333333333333333333333
         # Catalyst system setup
         # Symbolic system parameters: rate constants 
         @parameters k[1:12]
@@ -98,77 +78,75 @@ module GolgiModels
         @species C(t)[1:nMax] M(t)[1:nMax] T(t)[1:nMax] 
         # Use these parameters and variables to define a reaction system 
         
+        # Initialise reaction system (within array so it's a mutable object for ease of later updating)
         system = [refreshSystem(nMax,C,M,T,k,t,true)]
+        
+        # Initialise ODE integrator (within array so it's a mutable object for ease of later updating)
         integODE = [refreshODEs(nMax,C,M,T,k,t,ksInit,system[1])]
 
+        # Initialise stochastic integrator and accompanying objects (within arrays so they are mutable objects for ease of later updating)
         pStoch           = Pair.(collect(k),ksInit)
         u₀MapStoch       = Pair.([collect(C); collect(M); collect(T)], zeros(Int32,3*nMax)) 
         discreteProblem  = [DiscreteProblem(system[1], u₀MapStoch, (0.0,Inf), pStoch)]
         jumpProblem      = [JumpProblem(system[1], discreteProblem[1], Direct(), save_positions=(false,false))] # Converts system to a set of MassActionJumps
         integStoch       = [init(jumpProblem[1], SSAStepper())]
-        # integStoch       = [refreshStoch(nMax,C,M,T,k,ksInit,system[1])]
-        
-# 33333333333333333333333
 
-       
-# 44444444444444444444444
+        # Setup important observables
+        # Observable value for whether or not system is currently running
+        isrunning = Observable(false)
+        # Obvservable vector of slider values 
+        ksObservable = lift([s.value for s in parameterSliders.sliders]...) do values...
+            [values...]
+        end
         # Set up observable objects for cis, med, and trans results
         deterministicCisObservable = Observable(zeros(Float32, nMax))
         deterministicMedObservable = Observable(zeros(Float32, nMax))
         deterministicTraObservable = Observable(zeros(Float32, nMax))
-        dwellTimeObservable = Observable(zeros(Float32,7))
-        # Initialise plots
-        lines!(axCis, deterministicCisObservable, collect(1:nMax), color=(:red,1.0),   linewidth=6)
-        lines!(axMed, deterministicMedObservable, collect(1:nMax), color=(:green,1.0), linewidth=6)
-        lines!(axTra, deterministicTraObservable, collect(1:nMax), color=(:blue,1.0),  linewidth=6)
+        dwellTimeObservable        = Observable(zeros(Float32,7))
         # Set up observable objects for cis, med, and trans results
         stochasticCisObservable = Observable(zeros(Int32, nMax))
         stochasticMedObservable = Observable(zeros(Int32, nMax))
         stochasticTraObservable = Observable(zeros(Int32, nMax))
-        # Initialise plots
-        barplot!(axCis, collect(1:nMax), stochasticCisObservable, direction=:x, bins=collect(0.5:1.0:nMax+0.5), color=:red)
-        barplot!(axMed, collect(1:nMax), stochasticMedObservable, direction=:x, bins=collect(0.5:1.0:nMax+0.5), color=:green)
-        barplot!(axTra, collect(1:nMax), stochasticTraObservable, direction=:x, bins=collect(0.5:1.0:nMax+0.5), color=:blue)
-
-        barplot!(axDwell, collect(1:7), dwellTimeObservable, direction=:y, bins=collect(0.5:1.0:nMax+0.5), color=:blue)
-
         # Observables for stochastic time averages
         stochTimeAvCisObservable = Observable(zeros(Float32, nMax))
         stochTimeAvMedObservable = Observable(zeros(Float32, nMax))
         stochTimeAvTraObservable = Observable(zeros(Float32, nMax))
+
+# 44444444444444444444444
+        
         # Initialise plots
+        lines!(axCis, deterministicCisObservable, collect(1:nMax), color=(:red,1.0),   linewidth=6)
+        lines!(axMed, deterministicMedObservable, collect(1:nMax), color=(:green,1.0), linewidth=6)
+        lines!(axTra, deterministicTraObservable, collect(1:nMax), color=(:blue,1.0),  linewidth=6)
+        barplot!(axCis, collect(1:nMax), stochasticCisObservable, direction=:x, bins=collect(0.5:1.0:nMax+0.5), color=:red)
+        barplot!(axMed, collect(1:nMax), stochasticMedObservable, direction=:x, bins=collect(0.5:1.0:nMax+0.5), color=:green)
+        barplot!(axTra, collect(1:nMax), stochasticTraObservable, direction=:x, bins=collect(0.5:1.0:nMax+0.5), color=:blue)
         lines!(axCis, stochTimeAvCisObservable, collect(1:nMax), color=(:black,0.5), linestyle="--", linewidth=6)
         lines!(axMed, stochTimeAvMedObservable, collect(1:nMax), color=(:black,0.5), linestyle="--", linewidth=6)
         lines!(axTra, stochTimeAvTraObservable, collect(1:nMax), color=(:black,0.5), linestyle="--", linewidth=6)
-
+        barplot!(axDwell, collect(1:7), dwellTimeObservable, direction=:y, bins=collect(0.5:1.0:nMax+0.5), color=:blue)
         dwellTimesValues = zeros(Float32,7)
 
-# 44444444444444444444444        
-        
-# 55555555555555555555555
-        # Set up button actions 
-        isrunning = Observable(false)
+
+        # Set up button actions
         on(run.clicks) do clicks
             isrunning[] = !isrunning[]
         end
-        on(reset.clicks) do clicks    
-            isrunning[] = false            
-            # resetStepODE!(integODE,nMax,linearityToggleVal)
-            # resetStepStoch!(pStoch,u₀MapStoch,nMax,discreteProblem,tMax,jumpProblem,integStoch,C,M,T,k,ksInit,system,linearityToggleVal)
-            # resetObservables(axCis,axMed,axTra,nMax,deterministicCisObservable,deterministicMedObservable,deterministicTraObservable,stochasticCisObservable,stochasticMedObservable,stochasticTraObservable,stochTimeAvCisObservable,stochTimeAvMedObservable,stochTimeAvTraObservable,dwellTimeObservable)
+
+        on(reset.clicks) do clicks
+            isrunning[] = false
+            # Reset reaction system dependent on value of linearity toggle
             system[1] = refreshSystem(nMax,C,M,T,k,t,linearityToggle.active[])
-            integODE[1] = refreshODEs(nMax,C,M,T,k,t,ksInit,system[1])            
+            # Reset ODE integrator with new reaction system
+            integODE[1] = refreshODEs(nMax,C,M,T,k,t,ksInit,system[1])
+            # Reset stochastic integrator with new reaction system
             integStoch[1] = refreshStoch!(pStoch,u₀MapStoch,discreteProblem,jumpProblem,zeros(Int32,3*nMax),C,M,T,k,ksInit.*kStochFactors,system[1])
+            # Return sliders to original values
             for i_slider in parameterSliders.sliders
                 set_close_to!(i_slider, i_slider.startvalue[])
             end
+            # Refresh all plots
             resetObservables(axCis,axMed,axTra,nMax,deterministicCisObservable,deterministicMedObservable,deterministicTraObservable,stochasticCisObservable,stochasticMedObservable,stochasticTraObservable,stochTimeAvCisObservable,stochTimeAvMedObservable,stochTimeAvTraObservable,dwellTimeObservable)
-        end
-# 55555555555555555555555
-        
-
-        kObsVec = lift([s.value for s in parameterSliders.sliders]...) do values...
-            [values...]
         end
 
 
@@ -176,31 +154,30 @@ module GolgiModels
             while isrunning[]
                 isopen(fig.scene) || break # ensures computations stop if closed window
 
-                integODE[1].p .= kObsVec[] 
+                integODE[1].p .= ksObservable[]
                 animStepODE!(integODE[1],dt,axCis,axMed,axTra,deterministicCisObservable,deterministicMedObservable,deterministicTraObservable,nMax,V)
  
-                
                 hattedConstants!(integODE[1].p,k̂,integODE[1].u,nMax)
+
+        #   Improve this block; remove need for dwellTimesValues array
                 for i=1:7
                     dwellTimesValues[i] = -dwellTimesFuns[i](k̂)
                 end
                 dwellTimeObservable[] .= dwellTimesValues
                 dwellTimeObservable[] = dwellTimeObservable[]
                 ylims!(axDwell,(0,maximum(dwellTimesValues)))
-                
+        ###########
 
-                # integStoch[1] = refreshStoch(nMax,C,M,T,k,kStochFactors.*kObsVec[],system[1])
-                # pStoch = Pair.(collect(kSyms),kStochFactors.*kObsVec[])
-                # discreteProblem  = [DiscreteProblem(system, integStoch[1].u, (0.0,Inf), pStoch)]
-                # jumpProblem   = [JumpProblem(system, discreteProblem[1], Direct(), save_positions=(false,false))] # Converts system to a set of MassActionJumps
-                integStoch[1] = refreshStoch!(pStoch,u₀MapStoch,discreteProblem,jumpProblem,integStoch[1].u,C,M,T,k,kStochFactors.*kObsVec[],system[1])
+                integStoch[1] = refreshStoch!(pStoch,u₀MapStoch,discreteProblem,jumpProblem,integStoch[1].u,C,M,T,k,kStochFactors.*ksObservable[],system[1])
                 animStepStoch!(integStoch[1],dt,axCis,axMed,axTra,stochasticCisObservable,stochasticMedObservable,stochasticTraObservable,stochTimeAvCisObservable,stochTimeAvMedObservable,stochTimeAvTraObservable,nMax)
-                
+
+        #   Improve this block; remove need for xLimTimeAv array
                 # Find time averaged maximum value to set xlim
                 xLimTimeAv[1] = (xLimTimeAv[1]*19+max(maximum(integStoch[1].u),maximum(integODE[1].u)))/20
                 xlims!(axCis,(0.0,1.1*xLimTimeAv[1]))
                 xlims!(axMed,(0.0,1.1*xLimTimeAv[1]))
-                xlims!(axTra,(0.0,1.1*xLimTimeAv[1]))                
+                xlims!(axTra,(0.0,1.1*xLimTimeAv[1]))
+        ###########           
 
                 sleep(0.1)
             end
